c05940d97de806050faf0a5b96851e8997137fdf
Hugh Willson <hugh@octonary.com>
2016-07-20 15:45:51 -0400

    Wired up quick essence test.

---
+++ README.md
@@ -0,0 +1,3 @@
+# meteor-forum-26748-react-essence
+
+https://forums.meteor.com/t/how-to-set-up-meteor-react-essence/26748
---
+++ imports/ui/components/Widgets.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Icon from 'essence-icon';
+
+export const Widgets = () => (
+  <Icon name={'navigation-apps'} className={'e-text-indigo-500'} />
+);
